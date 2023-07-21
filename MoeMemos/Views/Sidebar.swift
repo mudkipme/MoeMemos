9672cb98b85ac5a48b147aee088dee30cea72383
lucka-me <i@lucka.moe>
2023-07-21 22:10:35 +0800

    Fix the Heatmap overflowing on some devices - Correct the calculation of columns, the gridSpacing should be concerned - Align the grids to trailing edge in sidebar and center in widget

--- MoeMemos/Views/Sidebar.swift
+++ MoeMemos/Views/Sidebar.swift
@@ -32,7 +32,7 @@ struct Sidebar: View {
                         Text(weekDaySymbols.last ?? "")
                             .font(.footnote).foregroundStyle(.secondary)
                     }
-                    Heatmap(matrix: memosViewModel.matrix)
+                    Heatmap(matrix: memosViewModel.matrix, alignment: .trailing)
                 }
                 .frame(minHeight: 120, maxHeight: 120)
                 .padding(.bottom, 10)
