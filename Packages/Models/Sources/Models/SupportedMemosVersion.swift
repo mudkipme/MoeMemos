9997379e9b27e234c45fdac0950fac917ccdd64c
Mudkip <i@mudkip.me>
2026-02-23 22:14:12 +0800

    chore: mark 0.26.2 as supported

--- Packages/Models/Sources/Models/SupportedMemosVersion.swift
+++ Packages/Models/Sources/Models/SupportedMemosVersion.swift
@@ -10,7 +10,7 @@ import Foundation
 public enum SupportedMemosVersion {
     public static let minimumV0 = "0.21.0"
     public static let minimumV1 = "0.26.0"
-    public static let maximumV1 = "0.26.1"
+    public static let maximumV1 = "0.26.2"
 
     public static func localizedSupportedVersionsMessage() -> String {
         let format = NSLocalizedString("compat.supported-versions", comment: "Supported Memos versions for Moe Memos")
